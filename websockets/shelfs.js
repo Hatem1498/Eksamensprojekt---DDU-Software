c6d98fbb3ac71eadca7e43a8bc7f9718b5d5ab27
Superkai <hatemghaith13@gmail.com>
2022-04-26 11:44:58 +0200

    s

--- websockets/shelfs.js
+++ websockets/shelfs.js
@@ -19,6 +19,7 @@ module.exports = websocket_2;
 
 async function SortData(data){
     let values = data.split(",");
+    console.log(values);
     let shelf = values[0];
     let cheese = values[1];
     insert(shelf, cheese);
