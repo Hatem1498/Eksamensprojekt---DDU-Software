61d7c4c0481c8f11f7989d4f70e6ac737a52c218
Superkai <hatemghaith13@gmail.com>
2022-05-03 12:54:01 +0200

    s

--- public/js/Konfig.js
+++ public/js/Konfig.js
@@ -10,15 +10,10 @@ function configure(event){
     let v_sensor_1 = document.querySelector("#t_sensor_1").value;
     data = {ssid: v_ssid, password: v_password, t_sensor_1: v_sensor_1};
     
-    console.log(data);
-
     data = SortObject(data);
-
-    console.log(data);
-
-    
-    
 }
+
+console.log(data);
 
 function SortObject(obj){
    let n_obj = {};
