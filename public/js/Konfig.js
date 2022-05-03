6393971e83031475ff4dc0a857b061ab64d77227
Superkai <hatemghaith13@gmail.com>
2022-05-03 11:34:26 +0200

    s

--- public/js/Konfig.js
+++ public/js/Konfig.js
@@ -8,7 +8,7 @@ function configure(event){
     let v_ssid = document.querySelector("#ssid").value;
     let v_password = document.querySelector("#password").value;
     let v_sensor_1 = document.querySelector("#t_sensor_1").value;
-    let data = {ssid: v_ssid, password: v_password, t_sensor_1: v_sensor_1};
+    data = {ssid: v_ssid, password: v_password, t_sensor_1: v_sensor_1};
     
     console.log(data);
 
