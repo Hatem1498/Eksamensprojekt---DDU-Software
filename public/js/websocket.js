88e44818e0e98fd88fe0d4c959bb174fa7f772fa
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-08 22:59:44 +0200

    s

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,53 +1,53 @@
-function websocket(){
-    console.log("Connecting...");
-    let HOST = location.origin.replace(/^http/, "ws");
-    let ws = new ReconnectingWebSocket(HOST);
-    
-    ws.onopen = (ev) => {
-        ws.send("site");
 
-        if(document.URL.includes("Graphs.html")){
-            ws.send("fetch");
-        }
-    }
+console.log("Connecting...");
+let HOST = location.origin.replace(/^http/, "ws");
+let ws = new ReconnectingWebSocket(HOST);
+
+ws.onopen = (ev) => {
+    ws.send("site");
 
     if(document.URL.includes("Graphs.html")){
-        setInterval(()=>{
-            ws.send("fetch");
-        }, 10000);
+        ws.send("fetch");
     }
-
-    ws.onmessage = (event) => {
-
-        //If data from the event is a Json, then the temp and hum are updated for the chart, since the Json would contain data stored in the database from the sensors.
-        if(isJson(event.data)){
-            let time = [];
-            let temp = [];
-            let hum = [];
-            let json = JSON.parse(event.data);
-            let refpoint = (new Date((json.filter(obj=>{return obj.id == 1}))[0].date_time)).getTime();
-            for(obj in json){
-                temp.push(json[obj].temp);
-                hum.push(json[obj].hum);
-                let date_time = parseInt((((new Date(json[obj].date_time)).getTime())-refpoint)/1000);
-                time.push(date_time);
-            }
-            updateChart(temp, hum, time);
-           
-        }
-        
-    };
-    setTimeout(()=>{
-        ws.close();
-    }, 12000);
-
-    ws.onclose = ()=>{
-        console.log("Connection closed...Restarting");
-    };
-
 }
 
-websocket();
+if(document.URL.includes("Graphs.html")){
+    setInterval(()=>{
+        ws.send("fetch");
+    }, 10000);
+}
+
+ws.onmessage = (event) => {
+
+    //If data from the event is a Json, then the temp and hum are updated for the chart, since the Json would contain data stored in the database from the sensors.
+    if(isJson(event.data)){
+        let time = [];
+        let temp = [];
+        let hum = [];
+        let json = JSON.parse(event.data);
+        let refpoint = (new Date((json.filter(obj=>{return obj.id == 1}))[0].date_time)).getTime();
+        for(obj in json){
+            temp.push(json[obj].temp);
+            hum.push(json[obj].hum);
+            let date_time = parseInt((((new Date(json[obj].date_time)).getTime())-refpoint)/1000);
+            time.push(date_time);
+        }
+        updateChart(temp, hum, time);
+        
+    }
+    
+};
+setTimeout(()=>{
+    ws.close();
+}, 12000);
+
+ws.onclose = ()=>{
+    console.log("Connection closed...Restarting");
+};
+
+
+
+
 
 
 
