942b45984fb17e90d2b061f9743c437ecbed3cd5
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 20:54:29 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -5,31 +5,32 @@ for(let i = 0; i < count; i++){
     let canvas = document.createElement('canvas');
     canvas.id = `box-${i}`;
     canvas.className = "shelf";
-    let p_left = parseInt(document.getElementById(`box-${i-1}`).style.left.replace("px", ""));
-    let p_width = parseInt(document.getElementById(`box-${i-1}`).style.width.replace("px", ""));
-    
     canvas.style.position = "absolute";
     if(i == 0 || i == 4 || i == 8 || i == 12){
         canvas.style.left = "20px";
     } 
     else{
+        let p_left = parseInt(document.getElementById(`box-${i-1}`).style.left.replace("px", ""));
+        let p_width = parseInt(document.getElementById(`box-${i-1}`).style.width.replace("px", ""));
         canvas.style.left = `${(p_left+20)+p_width}px`;
     }
     let w = $(window).width();
     canvas.style.width = `${w/4-20*6}px`;
     canvas.style.height = `${Math.ceil(w/4-20*6-50)}px`;
     canvas.style.top = "75px";
-
+    //Row 2
     if(i > 3){
         let p_height = parseInt(document.getElementById(`box-${0}`).style.height.replace("px", ""));
         let p_top = parseInt(document.getElementById(`box-${0}`).style.height.replace("px", ""));
         canvas.style.top = `${p_height+p_top+25}px`;
     }
+    //Row 3
     if(i > 7){
         let p_height = parseInt(document.getElementById(`box-${4}`).style.height.replace("px", ""));
         let p_top = parseInt(document.getElementById(`box-${4}`).style.height.replace("px", ""));
         canvas.style.top = `${p_height+p_top+25}px`;
     }
+    //Row 4
     if(i > 11){
         let p_height = parseInt(document.getElementById(`box-${8}`).style.height.replace("px", ""));
         let p_top = parseInt(document.getElementById(`box-${8}`).style.height.replace("px", ""));
