d521809d98548d79d56f2b849db13922ed8a0df3
Superkai <hatemghaith13@gmail.com>
2022-05-09 08:08:36 +0200

    s

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,48 +1,51 @@
+function websocket(){
+    console.log("Connecting...");
+    let HOST = location.origin.replace(/^http/, "ws");
+    let ws = new WebSocket(HOST);
 
-console.log("Connecting...");
-let HOST = location.origin.replace(/^http/, "ws");
-let ws = new ReconnectingWebSocket(HOST);
+    ws.onopen = (ev) => {
+        ws.send("site");
 
-ws.onopen = (ev) => {
-    ws.send("site");
+        if(document.URL.includes("Graphs.html")){
+            ws.send("fetch");
+        }
+    }
 
     if(document.URL.includes("Graphs.html")){
-        ws.send("fetch");
+        setInterval(()=>{
+            ws.send("fetch");
+        }, 10000);
     }
-}
 
-if(document.URL.includes("Graphs.html")){
-    setInterval(()=>{
-        ws.send("fetch");
-    }, 10000);
-}
+    ws.onmessage = (event) => {
 
-ws.onmessage = (event) => {
-
-    //If data from the event is a Json, then the temp and hum are updated for the chart, since the Json would contain data stored in the database from the sensors.
-    if(isJson(event.data)){
-        let time = [];
-        let temp = [];
-        let hum = [];
-        let json = JSON.parse(event.data);
-        let refpoint = (new Date((json.filter(obj=>{return obj.id == 1}))[0].date_time)).getTime();
-        for(obj in json){
-            temp.push(json[obj].temp);
-            hum.push(json[obj].hum);
-            let date_time = parseInt((((new Date(json[obj].date_time)).getTime())-refpoint)/1000);
-            time.push(date_time);
+        //If data from the event is a Json, then the temp and hum are updated for the chart, since the Json would contain data stored in the database from the sensors.
+        if(isJson(event.data)){
+            let time = [];
+            let temp = [];
+            let hum = [];
+            let json = JSON.parse(event.data);
+            let refpoint = (new Date((json.filter(obj=>{return obj.id == 1}))[0].date_time)).getTime();
+            for(obj in json){
+                temp.push(json[obj].temp);
+                hum.push(json[obj].hum);
+                let date_time = parseInt((((new Date(json[obj].date_time)).getTime())-refpoint)/1000);
+                time.push(date_time);
+            }
+            updateChart(temp, hum, time);
+            
         }
-        updateChart(temp, hum, time);
         
-    }
-    
-};
+    };
 
 
-ws.onclose = ()=>{
-    console.log("Connection closed...Restarting");
-};
-
+    ws.onclose = ()=>{
+        console.log("Connection closed...Restarting");
+        setTimeout(()=>{
+            websocket();
+        }, 1000);
+    };
+}
 
 setTimeout(()=> {ws.close()}, 2000)
 
