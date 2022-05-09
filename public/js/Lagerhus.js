725d1494b4a8cb1e58d82a14f6d936a82be1f279
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 19:04:42 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -1,7 +1,7 @@
 let element = document.getElementById('box-1')
-let x = 117; let y = 0
+let x = 1000; let y = 0
 
-const position = { x: 117, y: 0 }
+const position = { x: 1000, y: 0 }
 
 interact(element).draggable({listeners:{start(event){console.log(event.type,event.target)},move(event){position.x+=event.dx;position.y+=event.dy;event.target.style.transform=`translate(${position.x}px,${position.y}px)`},}})
 element.addEventListener('click', ()=>{console.log(`top: ${element.style.top}, left: ${element.style.left} `)})
