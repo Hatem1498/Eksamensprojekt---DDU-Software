35f18b785864eaa565564f9e4daffb01e5a3e599
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 21:53:13 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -2,15 +2,15 @@ let count = 16;
 const w = $(window).width();
 for(let i = 0; i < count; i++){
     let canvas = document.createElement('canvas');
-    canvas.id = `box-${i}`;
+    canvas.id = `shelf-${i}`;
     canvas.className = "shelf";
     canvas.style.position = "absolute";
     if(i == 0 || i == 4 || i == 8 || i == 12){
         canvas.style.left = "20px";
     } 
     else{
-        let p_left = parseFloat(document.getElementById(`box-${i-1}`).style.left.replace("px", ""));
-        let p_width = parseFloat(document.getElementById(`box-${i-1}`).style.width.replace("px", ""));
+        let p_left = parseFloat(document.getElementById(`shelf-${i-1}`).style.left.replace("px", ""));
+        let p_width = parseFloat(document.getElementById(`shelf-${i-1}`).style.width.replace("px", ""));
         canvas.style.left = `${(p_left+20)+p_width}px`;
     }
     canvas.style.width = `${w/4-20*5}px`;
@@ -19,22 +19,22 @@ for(let i = 0; i < count; i++){
     //Row 2
     if(i > 3 && i < 8){
         console.log(i);
-        let p_height = parseFloat(document.getElementById(`box-${0}`).style.height.replace("px", ""));
-        let p_top = parseFloat(document.getElementById(`box-${0}`).style.top.replace("px", ""));
+        let p_height = parseFloat(document.getElementById(`shelf-${0}`).style.height.replace("px", ""));
+        let p_top = parseFloat(document.getElementById(`shelf-${0}`).style.top.replace("px", ""));
         canvas.style.top = `${p_height+p_top+125}px`;
     }
     //Row 3
     else if(i > 7 && i < 12){
         console.log(i);
-        let p_height_1 = parseFloat(document.getElementById(`box-${4}`).style.height.replace("px", ""));
-        let p_top_1 = parseFloat(document.getElementById(`box-${4}`).style.top.replace("px", ""));
+        let p_height_1 = parseFloat(document.getElementById(`shelf-${4}`).style.height.replace("px", ""));
+        let p_top_1 = parseFloat(document.getElementById(`shelf-${4}`).style.top.replace("px", ""));
         canvas.style.top = `${p_height_1+p_top_1+125}px`;
     }
     //Row 4
     else if(i > 11 && i < 16){
         console.log(i);
-        let p_height_2 = parseFloat(document.getElementById(`box-${8}`).style.height.replace("px", ""));
-        let p_top_2 = parseFloat(document.getElementById(`box-${8}`).style.top.replace("px", ""));
+        let p_height_2 = parseFloat(document.getElementById(`shelf-${8}`).style.height.replace("px", ""));
+        let p_top_2 = parseFloat(document.getElementById(`shelf-${8}`).style.top.replace("px", ""));
         canvas.style.top = `${p_height_2+p_top_2+125}px`;
     }
     document.body.appendChild(canvas);
