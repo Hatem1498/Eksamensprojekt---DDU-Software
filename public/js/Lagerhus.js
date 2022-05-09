1465584c92fa10181519a4236fe68aada1554d36
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 19:53:14 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -1,5 +1,6 @@
 let count = 16;
-
+const position = { x: 0, y: 0 }
+let x = 0; let y = 0
 for(let i = 0; i < count; i++){
     let canvas = document.createElement('canvas');
     canvas.id = `box-${i}`;
@@ -8,16 +9,17 @@ for(let i = 0; i < count; i++){
     canvas.style.width = `${w/4-10}px`;
     canvas.style.height = `${Math.ceil(w/4-50)}px`;
     document.body.appendChild(canvas);
+    interact(canvas).draggable({listeners:{start(event){console.log(event.type,event.target)},move(event){position.x+=event.dx;position.y+=event.dy;event.target.style.transform=`translate(${position.x}px,${position.y}px)`},}})
 }
 
 let element = document.getElementById('box-1');
-let x = 0; let y = 0
 
 
 
 
 
-const position = { x: 0, y: 0 }
 
-interact(element).draggable({listeners:{start(event){console.log(event.type,event.target)},move(event){position.x+=event.dx;position.y+=event.dy;event.target.style.transform=`translate(${position.x}px,${position.y}px)`},}})
+
+
+
 element.addEventListener('click', ()=>{console.log(`top: ${element.style.top}, left: ${element.style.left} `)})
