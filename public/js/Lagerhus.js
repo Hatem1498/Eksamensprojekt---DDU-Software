b101ca2de2041fbebc6cb9928f86148d056b8cab
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 19:19:56 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -1,6 +1,10 @@
 let element = document.getElementById('box-1')
 let x = 0; let y = 0
 
+let w = $(window).width();
+
+element.style.width = w/4-25;
+
 const position = { x: 0, y: 0 }
 
 interact(element).draggable({listeners:{start(event){console.log(event.type,event.target)},move(event){position.x+=event.dx;position.y+=event.dy;event.target.style.transform=`translate(${position.x}px,${position.y}px)`},}})
