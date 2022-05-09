e9f42f93e132185f5053a6bb721477fed108a1cb
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-09 20:22:09 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -12,7 +12,7 @@ for(let i = 0; i < count; i++){
     canvas.style.width = `${w/4-20*6}px`;
     canvas.style.height = `${Math.ceil(w/4-20*6-50)}px`;
     canvas.style.top = "75px";
-    canvas.style.position = "absolute";
+    canvas.style.position = "relative";
     if(i > 0){
     let pre = parseInt(document.getElementById(`box-${i-1}`).style.left.replace("px", ""));
     canvas.style.left = `${pre+20}px`
