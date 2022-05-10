35be7735a784d004f1170bb5e0bb8d24b7321d7d
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-10 22:01:46 +0200

    s

--- public/js/Konfig.js
+++ public/js/Konfig.js
@@ -2,6 +2,15 @@ const form = document.getElementsByClassName("Konfig")[0];
 
 form.addEventListener("submit", configure);
 
+setInterval(() => {
+    let v_ssid = document.querySelector("#ssid").value;
+    let v_password = document.querySelector("#password").value;
+    let v_sensor_1 = document.querySelector("#t_sensor_1").value;
+    let data = {ssid: v_ssid, password: v_password, t_sensor_1: v_sensor_1};
+    
+    data = SortObject(data);
+    console.log(JSON.stringify(data));
+}, 5000);
 
 function configure(event){
     let v_ssid = document.querySelector("#ssid").value;
