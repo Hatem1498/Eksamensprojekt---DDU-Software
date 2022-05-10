ad46172237c37f2857a20269dc179d88cc61e5d0
Hatem Ghaith <hatemghaith13@gmail.com>
2022-05-10 08:50:30 +0200

    s

--- public/js/Lagerhus.js
+++ public/js/Lagerhus.js
@@ -1,4 +1,4 @@
-let count = 16;
+/* let count = 16;
 const w = $(window).width();
 for(let i = 0; i < count; i++){
     //create canvas
@@ -45,5 +45,34 @@ for(let i = 0; i < count; i++){
     }
     //Finally append the canvas element into the body element of html
     document.body.appendChild(canvas);
+    
+} */
+
+for(let i = 0; i < 4; i++){
+    
+    for(let j = 0; j < 4; j++){
+        let canvas = document.createElement('canvas');
+        canvas.id = `shelf-${j+(i*4)}`;
+        canvas.className = "shelf";
+        canvas.style.position = "absolute";
+        if(j == 0){
+            canvas.style.left = "20px";
+        }
+        else{
+            let p_left = parseFloat(document.getElementById(`shelf-${j+(i*4)-1}`).style.left.replace("px", ""));
+            let p_width = parseFloat(document.getElementById(`shelf-${j+(i*4)-1}`).style.width.replace("px", ""));
+            canvas.style.left = `${(p_left+20)+p_width}px`;
+        }
+        canvas.style.width = `${(w/4-40)}px`;
+        canvas.style.height = `${(w/4-40)/2-50}px`;
+        canvas.style.top = "75px";
+        
+        if(i > 0){
+            let p_height = parseFloat(document.getElementById(`shelf-${i-1}`).style.height.replace("px", ""));
+            let p_top = parseFloat(document.getElementById(`shelf-${i-1}`).style.top.replace("px", ""));
+            canvas.style.top = `${p_height+p_top+100}px`;
+        }
+        document.body.appendChild(canvas);
+    }
     
 }
